3f1d65946a01e62e9be50da8910c59123510c502
phuyent <43248395+phuyent@users.noreply.github.com>
2022-08-31 23:11:36 -0400

    FinalProject-part1
    Adding all the files, libraries needed for the base

---
+++ FinalProject/js/script.js
@@ -0,0 +1,10 @@
+
+
+"use strict";
+
+//Constants for color coding in THREE.js library
+const white = new THREE.Color( 0xffffff );
+const black = new THREE.Color( 0x000000 );
+const red = new THREE.Color( 0xff0000 );
+const green = new THREE.Color( 0x00ff00 );
+const blue = new THREE.Color( 0x0000ff );
